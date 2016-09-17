83d4ce96ee706add539d4a2d4f706bc1ebf6778c
Zachary Hill <zachary13hill@yahoo.com>
2016-09-17 15:41:07 -0400

    Initial make setup

---
+++ prog1.c
@@ -0,0 +1,17 @@
+/**
+ * Zachary Hill
+ * CS 3331
+ *
+ * This is an introductory program to the fork() and wait() functions
+ * This program fork()s 4 children to run separate calculations.
+ */
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main(int argc, char* argv[]) {
+
+    printf("Hello World\n");
+
+}
