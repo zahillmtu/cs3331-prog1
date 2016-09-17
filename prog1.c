5a7819e765e7f774ffd87a1993ba755ba6d8c031
Zachary Hill <zachary13hill@yahoo.com>
2016-09-17 16:15:11 -0400

    methods for each fork() and printing with write

--- prog1.c
+++ prog1.c
@@ -10,6 +10,39 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<string.h>
+
+void printWrap(char buf[100]) {
+    write(1, buf, strlen(buf));
+}
+
+void randomNums(int m) {
+    char buf[100];
+
+    sprintf(buf, "Random number has int %d\n", m);
+    printWrap(buf);
+}
+
+void fibonacci(int n) {
+    char buf[100];
+
+    sprintf(buf, "fibonacci has int %d\n", n);
+    printWrap(buf);
+}
+
+void needleSim(int r) {
+    char buf[100];
+
+    sprintf(buf, "needleSim has int %d\n", r);
+    printWrap(buf);
+}
+
+void integration(int s) {
+    char buf[100];
+
+    sprintf(buf, "Integration has int %d\n", s);
+    printWrap(buf);
+}
 
 int main(int argc, char* argv[]) {
 
@@ -22,4 +55,10 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < 5; i++) {
         printf("%d\n", atoi(argv[i]));
     }
+
+    randomNums(atoi(argv[1]));
+    fibonacci(atoi(argv[2]));
+    needleSim(atoi(argv[3]));
+    integration(atoi(argv[4]));
+    exit(0);
 }
