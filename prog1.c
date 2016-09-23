685117230b480f6c489a2946ad8b1fa7e2400bd4
Zachary Hill <zachary13hill@yahoo.com>
2016-09-23 13:52:28 -0400

    Finished coding but needs comments and checking

--- prog1.c
+++ prog1.c
@@ -15,6 +15,7 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
 #define PI 3.14159265358979323846
 
@@ -31,21 +32,121 @@ void printWrap(char buf[100]) {
     write(1, buf, strlen(buf));
 }
 
+void downheap(int numbers[], int numbersSize, int index) {
+
+    int leftIndex;
+    int rightIndex;
+    int largeChildIndex;
+    int temp;
+    int index2 = index; // copy of index for modifing
+
+    while((2 * index2 + 1) < numbersSize) { // while has left child
+        leftIndex = 2 * index2 + 1; // index of left child
+        largeChildIndex = leftIndex;
+        if ((2 * index2 + 2) < numbersSize) { // if has right child
+            rightIndex = 2 * index2 + 2;
+            if (numbers[rightIndex] < numbers[leftIndex]) {
+                largeChildIndex = rightIndex;
+            }
+        }
+        if (numbers[largeChildIndex] > numbers[index2]) { // done swapping
+            break;
+        }
+        // swap the elements
+        temp = numbers[index2];
+        numbers[index2] = numbers[largeChildIndex];
+        numbers[largeChildIndex] = temp;
+        index2 = largeChildIndex;
+    }
+}
+
+
+void heapify(int numbers[], int numbersSize) {
+
+    // Build the heap property
+    for (int i = numbersSize / 2; i >= 0; i--) {
+        downheap(numbers, numbersSize, i);
+    }
+}
+
+
+void heapSort(int numbers[], int numbersSize) {
+    int temp;
+    int heapSize = numbersSize;
+
+    // create the heap
+    heapify(numbers, numbersSize);
+    // repeatedly swap the largest to the front of the array
+    for (int i = numbersSize - 1; i > 0; i--) {
+        temp = numbers[0];
+        numbers[0] = numbers[i];
+        numbers[i] = temp;
+        heapSize = heapSize - 1;
+        // rebuild the heap property
+        downheap(numbers, heapSize, 0);
+    }
+
+    // I made it sort backwards so to fix, reverse the array
+    int swapVar = numbersSize - 1;
+    for (int i = 0; i < numbersSize / 2; i++) {
+       temp = numbers[i];
+       numbers[i] = numbers[swapVar];
+       numbers[swapVar] = temp;
+       swapVar = swapVar - 1;
+    }
+}
+
 void randomNums(int m) {
     char buf[100];
     char intBuf[25];
     int numbers[m];
     int j;
 
+    memset(numbers, 0, m);
+
     sprintf(buf, "   Heap Sort Process Started\n");
     printWrap(buf);
+    if (m == 0) {
+        sprintf(buf, "   Random Numbers Generated:\n\n");
+        printWrap(buf);
+        sprintf(buf, "   Sorted Sequence:\n\n");
+        printWrap(buf);
+        sprintf(buf, "   Heap Sort Process Exits\n");
+        printWrap(buf);
+        exit(0);
+    }
+
 
     // Generate the random numbers
+    srand(time(NULL));
     for (int i = 0; i < m; i++) {
         numbers[i] = rand() % 100;
     }
 
+    sprintf(buf, "   Random Numbers Generated:\n");
+    printWrap(buf);
     // Print the random numbers generated
+    j = 0;
+    while (j < m) {
+        // print out the numbers 20 per line
+        sprintf(buf, "   "); // the indentation
+        for (int k = 0; k < 20; k++) {
+            if (j < m) {
+                sprintf(intBuf, "%4d ", numbers[j]);
+                strcat(buf, intBuf);
+                j++;
+            }
+        }
+        strcat(buf, "\n");
+        printWrap(buf);
+    }
+
+    // call heap sort
+    heapSort(numbers, m);
+
+    sprintf(buf, "   Sorted Sequence:\n");
+    printWrap(buf);
+    // Print the random numbers sorted
     j = 0;
     while (j < m) {
         // print out the numbers 20 per line
@@ -111,6 +212,7 @@ void needleSim(int r) {
     sprintf(buf, "         Buffon's Needle Process Started\n");
     printWrap(buf);
 
+    srand(time(NULL));
     for (int i = 0; i < r; i++) {
         dis = ((double) rand())/RAND_MAX;                // random number [0, 1)
         angle = (((double) rand())/RAND_MAX) * 2 * PI; // random number [0, 2pi)
@@ -141,6 +243,7 @@ void integration(int s) {
     double b;              // y-coordinate
     double finalRes;       // computed area
 
+    srand(time(NULL));
     for (int i = 0; i < s; i++) {
         b = ((double) rand())/RAND_MAX;        // random number [0, 1)
         a = (((double) rand())/RAND_MAX) * PI; // random number [0, pi)
@@ -167,6 +270,7 @@ void integration(int s) {
 
 int main(int argc, char* argv[]) {
 
+    char buf[100];
     pid_t pid;
 
     if (argc != 5) {
@@ -175,8 +279,21 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    sprintf(buf, "Main Process Started\n");
+    printWrap(buf);
+    sprintf(buf, "Number of Random Numbers    = %d\n", atoi(argv[1]));
+    printWrap(buf);
+    sprintf(buf, "Fibonacci Input             = %d\n", atoi(argv[2]));
+    printWrap(buf);
+    sprintf(buf, "Buffon's Needle Interations = %d\n", atoi(argv[3]));
+    printWrap(buf);
+    sprintf(buf, "Integration Interations     = %d\n", atoi(argv[4]));
+    printWrap(buf);
+
+
     for(int i = 0; i < 4; i++) {
         pid = fork();
+
 
         if (pid == -1) {
             perror("fork");
@@ -185,29 +302,60 @@ int main(int argc, char* argv[]) {
             switch(i) {
                 case 0:
                     randomNums(atoi(argv[1]));
+                    exit(0);
                     break;
 
                 case 1:
                     fibonacci(atoi(argv[2]));
+                    exit(0);
                     break;
 
                 case 2:
                     needleSim(atoi(argv[3]));
+                    exit(0);
                     break;
 
                 case 3:
                     integration(atoi(argv[4]));
+                    exit(0);
                     break;
             }
-            break;
+        }
+        else {
+            switch(i) {
+                case 0:
+                    sprintf(buf, "Heap Sort Process Created\n");
+                    printWrap(buf);
+                    break;
+
+                case 1:
+                    sprintf(buf, "Fibonacci Process Created\n");
+                    printWrap(buf);
+                    break;
+
+                case 2:
+                    sprintf(buf, "Buffon's Needle Process Created\n");
+                    printWrap(buf);
+                    break;
+
+                case 3:
+                    sprintf(buf, "Integration Process Created\n");
+                    printWrap(buf);
+                    break;
+            }
+
         }
     }
 
     int status;
 
+    sprintf(buf, "Main Process Waits\n");
+    printWrap(buf);
     for(int i = 0; i < 4; i++) {
         wait(&status);
     }
+    sprintf(buf, "Main Process Exits\n");
+    printWrap(buf);
 
     exit(0);
 }
